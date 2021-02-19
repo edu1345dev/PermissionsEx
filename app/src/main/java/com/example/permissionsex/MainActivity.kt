0abf084455775b1d8f18ae4777cb37d86ced5108
josesantos <jose.l.santos88@gmail.com>
2021-02-19 11:16:50 -0300

    encapsulate permissions and preference classes

--- app/src/main/java/com/example/permissionsex/MainActivity.kt
+++ app/src/main/java/com/example/permissionsex/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.permissionsex
 
 import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private val permissionsHelper = PermissionsHelper(this)
+    private val sharedPreferencesHelper = SharedPreferencesHelper(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initViews()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (shouldAskUserToGrantPermissions()){
+            //TODO show dialog asking user to grant permission
+        }
+    }
+
+    private fun shouldAskUserToGrantPermissions(): Boolean {
+        return false
     }
 
     private fun initViews() {
@@ -45,58 +55,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestPermission(permission: String) {
-        val isGranted = ContextCompat.checkSelfPermission(this, permission)
-
-        if (isGranted != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    arrayOf(permission),
-                    1)
-        } else {
-            Toast.makeText(this, "permission already granted", Toast.LENGTH_LONG).show()
-        }
+        permissionsHelper.requestPermission(permission)
     }
 
     private fun requestAllPermission() {
-        val permission1 = Manifest.permission.WRITE_EXTERNAL_STORAGE
-        val permission2 = Manifest.permission.CAMERA
-        val permission3 = Manifest.permission.ACCESS_FINE_LOCATION
-
-        val permissions = listOf(permission1, permission2, permission3)
-        val permissionsNotGranted = mutableListOf<String>()
-
-        permissions.forEach {
-            if (ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED) {
-                permissionsNotGranted.add(it)
-            }
-        }
-
-        if (permissionsNotGranted.isNotEmpty()) {
-            ActivityCompat.requestPermissions(this,
-                    permissionsNotGranted.toTypedArray(),
-                    2)
-        } else {
-            Toast.makeText(this, "all permission granted", Toast.LENGTH_LONG).show()
-        }
+        val permissions = listOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION)
+        permissionsHelper.requestAllPermission(permissions)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (permissions.isEmpty()) return
-
-        when (requestCode) {
-            1 -> {
-                if (shouldAskAgain(permissions[0])) {
-                    requestPermission(permissions[0])
-                }
-            }
-            else -> {
-                Toast.makeText(this, "${permissions[0]} permission granted - request code $requestCode", Toast.LENGTH_LONG).show()
-            }
-        }
+        permissionsHelper.handleRequestPermissionResult(requestCode, permissions, grantResults)
     }
-
-    private fun shouldAskAgain(permission: String) =
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                    shouldShowRequestPermissionRationale(permission)
 }
